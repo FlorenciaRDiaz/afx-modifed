03846ec30d8d508f18b1e5dbcfd0cf422d1a71cc
FlorenciaRDiaz <144918233+FlorenciaRDiaz@users.noreply.github.com>
2024-04-17 08:28:47 -0300

    Subiendo un archivo de python

---
+++ Run.py
@@ -0,0 +1,6 @@
+git clone https://github.com/AngieLCerrutti/AlphaFoldXplore
+pip install -r requirements.txt
+from AlphaFoldXplore import alphafoldxplore
+
+# Usa las funciones de AlphaFoldXplore según lo necesites
+
