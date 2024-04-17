13af037a660b80d882c8188c8431ba2dd70096d0
FlorDiaz <florencia_diaz@mi.unc.edu.ar>
2024-04-17 15:38:15 -0300

    cambiando algo del archivo Run.py

--- Run.py
+++ Run.py
@@ -1,6 +1,8 @@
-git clone https://github.com/AngieLCerrutti/AlphaFoldXplore
-pip install -r requirements.txt
-from AlphaFoldXplore import alphafoldxplore
+#git clone https://github.com/AngieLCerrutti/AlphaFoldXplore
+#pip install -r requirements.txt
+#from AlphaFoldXplore import alphafoldxplore
 
 # Usa las funciones de AlphaFoldXplore según lo necesites
-
+nombre="Flor"
+saludar= "Holu " + nombre + " ¿todo bien?"
+print(saludar)
